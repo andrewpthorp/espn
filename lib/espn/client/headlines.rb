b052309a5fe4815818611b4d9a5206966d4bef8e
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-26 15:47:32 -0400

    added support for #headline, updates to #headlines specs

--- lib/espn/client/headlines.rb
+++ lib/espn/client/headlines.rb
@@ -10,6 +10,22 @@ module ESPN
     #   end
     module Headlines
 
+      # Public: Get a specific headline from the ESPN API.
+      #
+      # id    - The id of the headline to get.
+      # opts  - Hash options used to refine the selection. You can find the full
+      #         list of options on the ESPN developer API website (default: {}).
+      #
+      # Examples:
+      #
+      #   client.headline(12345)
+      #   # Request made to 'sports/news/12345'
+      #
+      # Returns a Hashie::Mash.
+      def headline(id, opts={})
+        get("sports/news/#{id}", opts).headlines.first
+      end
+
       # Public: Get latest sports news and analysis from the ESPN API.
       #
       # sport   - A Symbol or String of the sport (optional).
@@ -18,12 +34,12 @@ module ESPN
       #           sport (optional).
       # opts    - Hash options used to refine the selection. If sport and/or
       #           league are passed in, they will override the mapped values
-      #           (default: {}).
+      #           You can find a full list of options on the ESPN developer API
+      #           website (default: {}).
       #           - :section  - The news section (default: 'sports').
       #           - :sport    - Name of the sport (default: nil, optional).
       #           - :league   - Name of the league (default: nil, optional).
       #           - :method   - Method of headlines (default: nil, optional).
-      #           - :id       - Specific headline ID (default: nil, optional).
       #
       # Examples
       #
@@ -53,7 +69,7 @@ module ESPN
         url = build_url(pattern, opts)
 
         # Make request
-        get(url, opts)
+        get(url, opts).headlines
       end
 
     end
--- spec/espn/client/headlines_spec.rb
+++ spec/espn/client/headlines_spec.rb
@@ -1,13 +1,42 @@
 require 'spec_helper'
 
+def stub_headlines(url)
+  results = Hashie::Mash.new(headlines: [Hashie::Mash.new(headline: 'First Headline')])
+  stub_get(url).to_return(body: results)
+end
+
 describe ESPN::Client::Headlines do
   before do
     @client = ESPN::Client.new
   end
 
+  describe '#headline' do
+    it 'should include the id in the request' do
+      stub_headlines('sports/news/5')
+      @client.headline(5)
+      assert_requested :get, espn_url('sports/news/5')
+    end
+
+    it 'should not return an array' do
+      stub_headlines('sports/news/5')
+      @client.headline(5).should_not be_a(Array)
+    end
+
+    it 'should return the headline' do
+      stub_headlines('sports/news/5')
+      @client.headline(5).headline.should == 'First Headline'
+    end
+  end
+
   describe '#headlines' do
+    it 'should return an array of headlines' do
+      stub_headlines('sports/news')
+      results = @client.headlines
+      results.first.headline.should eq('First Headline')
+    end
+
     it 'should get headlines for sports by default' do
-      stub_get('sports/news')
+      stub_headlines('sports/news')
       @client.headlines
       assert_requested :get, espn_url('sports/news')
     end
@@ -15,7 +44,7 @@ describe ESPN::Client::Headlines do
     context 'when passing values as args' do
       context 'when passing the sport' do
         it 'should request headlines for that sport' do
-          stub_get('sports/horse-racing/news')
+          stub_headlines('sports/horse-racing/news')
           @client.headlines('horse-racing')
           assert_requested :get, espn_url('sports/horse-racing/news')
         end
@@ -23,7 +52,7 @@ describe ESPN::Client::Headlines do
 
       context 'when passing the league' do
         it 'should request headlines for that league' do
-          stub_get('sports/baseball/mlb/news')
+          stub_headlines('sports/baseball/mlb/news')
           @client.headlines(:mlb)
           assert_requested :get, espn_url('sports/baseball/mlb/news')
         end
@@ -31,7 +60,7 @@ describe ESPN::Client::Headlines do
 
       context 'when passing the league and sport' do
         it 'should request headlines for that sport and league' do
-          stub_get('sports/horse-racing/mlb/news')
+          stub_headlines('sports/horse-racing/mlb/news')
           @client.headlines('horse-racing', 'mlb')
           assert_requested :get, espn_url('sports/horse-racing/mlb/news')
         end
@@ -39,7 +68,7 @@ describe ESPN::Client::Headlines do
 
       context 'when passing sport in the opts hash' do
         it 'should override the symbol with the opts' do
-          stub_get('sports/baseball/news')
+          stub_headlines('sports/baseball/news')
           @client.headlines(:basketball, sport: 'baseball')
           assert_requested :get, espn_url('sports/baseball/news')
         end
@@ -47,7 +76,7 @@ describe ESPN::Client::Headlines do
 
       context 'when passing league in the opts hash' do
         it 'should override the symbol with the opts' do
-          stub_get('sports/basketball/mlb/news')
+          stub_headlines('sports/basketball/mlb/news')
           @client.headlines('wnba', league: 'mlb')
           assert_requested :get, espn_url('sports/basketball/mlb/news')
         end
@@ -57,7 +86,7 @@ describe ESPN::Client::Headlines do
     context 'when passing values in the opts hash' do
       context 'with a section param' do
         it 'should get headlines for that section' do
-          stub_get('cities/news')
+          stub_headlines('cities/news')
           @client.headlines(section: 'cities')
           assert_requested :get, espn_url('cities/news')
         end
@@ -65,7 +94,7 @@ describe ESPN::Client::Headlines do
 
       context 'with a sport param' do
         it 'should include the sport in the request' do
-          stub_get('sports/baseball/news')
+          stub_headlines('sports/baseball/news')
           @client.headlines(sport: 'baseball')
           assert_requested :get, espn_url('sports/baseball/news')
         end
@@ -73,7 +102,7 @@ describe ESPN::Client::Headlines do
 
       context 'with a sport and league param' do
         it 'should include both params in the request' do
-          stub_get('sports/baseball/mlb/news')
+          stub_headlines('sports/baseball/mlb/news')
           @client.headlines(sport: 'baseball', league: 'mlb')
           assert_requested :get, espn_url('sports/baseball/mlb/news')
         end
@@ -81,7 +110,7 @@ describe ESPN::Client::Headlines do
 
       context 'with a league param but no sport param' do
         it 'should not include the league in the request' do
-          stub_get('sports/news')
+          stub_headlines('sports/news')
           @client.headlines(league: 'mlb')
           assert_requested :get, espn_url('sports/news')
         end
@@ -89,20 +118,12 @@ describe ESPN::Client::Headlines do
 
       context 'with a method param' do
         it 'should include the method in the request' do
-          stub_get('sports/news/headlines/top')
+          stub_headlines('sports/news/headlines/top')
           @client.headlines(method: 'top')
           assert_requested :get, espn_url('sports/news/headlines/top')
         end
       end
-
-      context 'with a id param' do
-        it 'should include the id in the request' do
-          stub_get('sports/news/5')
-          @client.headlines(id: 5)
-          assert_requested :get, espn_url('sports/news/5')
-        end
-      end
     end
-
   end
+
 end
