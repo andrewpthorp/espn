676d9a771672fd042a3f3ffebfdc5dc8c17c3158
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-27 13:57:17 -0400

    updates to athletes spec

--- spec/espn/client/athletes_spec.rb
+++ spec/espn/client/athletes_spec.rb
@@ -7,6 +7,19 @@ describe ESPN::Client::Athletes do
   end
 
   describe '#athletes' do
+    it 'should return an array of athletes' do
+      @client.athletes(:mlb).first.fullName.should eq('Chase Utley')
+    end
+
+    it 'should get athletes for the given league' do
+      @client.athletes(:nba)
+      assert_requested :get, espn_url('sports/basketball/nba/athletes')
+    end
+
+    it 'should accept a league in the opts hash' do
+      @client.athletes(league: 'mlb')
+      assert_requested :get, espn_url('sports/baseball/mlb/athletes')
+    end
 
     context 'when league is not passed' do
       it 'should raise an argument error' do
@@ -20,22 +33,11 @@ describe ESPN::Client::Athletes do
       end
     end
 
-    it 'should return an array of athletes' do
-      results = @client.athletes(:mlb)
-      results.first.fullName.should eq('Chase Utley')
-    end
-
-    it 'should accept a league in the opts hash' do
-      results = @client.athletes(league: 'mlb')
-      assert_requested :get, espn_url('sports/baseball/mlb/athletes')
-    end
-
     context 'when passing an id in the opts hash' do
       it 'should request only that athlete' do
-        results = @client.athletes(:mlb, id: 1)
+        @client.athletes(:mlb, id: 1)
         assert_requested :get, espn_url('sports/baseball/mlb/athletes/1')
       end
     end
-
   end
 end
