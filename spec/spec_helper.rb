1fb64e35334bc594eb76c5d87b3e30ff51c78300
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-27 11:14:42 -0400

    add hashie and #default_response to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'espn'
 require 'rspec'
 require 'webmock/rspec'
+require 'hashie'
 
 def stub_get(url)
   stub_request(:get, espn_url(url))
@@ -8,4 +9,12 @@ end
 
 def espn_url(url)
   "http://api.espn.com/v1/#{url}"
+end
+
+def default_response(key, node)
+  results = Hashie::Mash.new
+  results.sports = [Hashie::Mash.new]
+  results.sports.first.leagues = [Hashie::Mash.new]
+  results.sports.first.leagues.first[key] = [node]
+  return results
 end
