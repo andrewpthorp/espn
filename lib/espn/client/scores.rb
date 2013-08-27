70005ac8a191c626894f3d1c5c895ea4283115cf
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-27 19:45:45 -0400

    updates to #scores

--- lib/espn/client/scores.rb
+++ lib/espn/client/scores.rb
@@ -1,32 +1,40 @@
 module ESPN
   class Client
+
+    # Public: The mapping to the SCORES & SCHEDULES API of the ESPN developer
+    # API.
+    #
+    # Examples
+    #
+    #   class Client
+    #     include ESPN::Client::Scores
+    #   end
     module Scores
 
       # Public: Get schedule and scores information from the ESPN API.
       #
-      # opts  - Hash options used to refine the selection (default: {}).
-      #       - :sport    - The name of the sport (default: nil).
-      #       - :league   - The name of the league (default: nil).
-      #       - :event_id - The event to get information about (default: nil).
+      # sport   - A Symbol or String of the sport (optional).
+      # league  - A Symbol or String of the league. If a league is passed, but
+      #           not a sport, an attempt will be made to map the league to a
+      #           sport (required).
+      # opts    - Hash options used to refine the selection. If sport and/or
+      #           league are passed in, they will override the mapped values
+      #           You can find a full list of options on the ESPN developer API
+      #           website (default: {}).
+      #           - :id - The id of a specific event (default: nil, optional).
       #
       # Returns an Array of Hashie::Mash.
-      def scores(opts={})
-        url = 'sports'
+      def scores(*args)
+        # Build fragments and options
+        opts = extract_options_with_defaults!(args)
+        opts[:sport], opts[:league] = extract_sport_and_league(args, opts)
 
-        unless opts[:sport].to_s.empty?
-          url += "/#{opts[:sport]}"
-          unless opts[:league].to_s.empty?
-            url += "/#{opts[:league]}"
-          end
-        end
+        # Build URL
+        pattern = 'sports/:sport/:league/events/:id'
+        url = build_url(pattern, opts)
 
-        url += '/events'
-
-        unless opts[:event_id].to_s.empty?
-          url += "/#{opts[:event_id]}"
-        end
-
-        get(url, opts)
+        # Make request
+        get(url, opts).sports.first.leagues.first.events
       end
 
     end
