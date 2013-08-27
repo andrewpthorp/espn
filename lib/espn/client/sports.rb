1ad9cf46d659d8b6a563f0bfca11c5a2da97f6d4
Andrew Thorp <andrewpthorp@gmail.com>
2013-08-27 14:17:33 -0400

    updates to #sports

--- lib/espn/client/sports.rb
+++ lib/espn/client/sports.rb
@@ -1,25 +1,38 @@
 module ESPN
   class Client
+
+    # Public: The mapping to the SPORTS API of the ESPN developer API.
+    #
+    # Examples
+    #
+    #   class Client
+    #     include ESPN::Client::Sports
+    #   end
     module Sports
 
       # Public: Get sports and leagues supported in the ESPN API.
       #
-      # opts  - Hash options used to refine the selection (default: {}).
-      #       - :sport - The name of the sport (default: nil).
-      #       - :league - The name of the league (default: nil).
+      # sport   - A Symbol or String of the sport (optional).
+      # league  - A Symbol or String of the league. If a league is passed, but
+      #           not a sport, an attempt will be made to map the league to a
+      #           sport (required).
+      # opts    - Hash options used to refine the selection. If sport and/or
+      #           league are passed in, they will override the mapped values
+      #           You can find a full list of options on the ESPN developer API
+      #           website (default: {}).
       #
       # Returns an Array of Hashie::Mash.
-      def sports(opts={})
-        url = 'sports'
+      def sports(*args)
+        # Build fragments and options
+        opts = extract_options_with_defaults!(args)
+        opts[:sport], opts[:league] = extract_sport_and_league(args, opts)
 
-        unless opts[:sport].to_s.empty?
-          url += "/#{opts[:sport]}"
-          unless opts[:league].to_s.empty?
-            url += "/#{opts[:league]}"
-          end
-        end
+        # Build URL
+        pattern = 'sports/:sport/:league'
+        url = build_url(pattern, opts)
 
-        get(url, opts)
+        # Make request
+        get(url, opts).sports
       end
 
     end
--- spec/espn/client/sports_spec.rb
+++ spec/espn/client/sports_spec.rb
@@ -7,32 +7,73 @@ describe ESPN::Client::Sports do
   end
 
   describe '#sports' do
+    it 'returns an array of sports' do
+      @client.sports.first.name.should eq('baseball')
+    end
 
     it 'gets all sports' do
       @client.sports
       assert_requested :get, espn_url('sports')
     end
 
-    context 'with a sport param' do
-      it 'should include the sport param in the request' do
-        @client.sports(sport: 'baseball')
-        assert_requested :get, espn_url('sports/baseball')
+    context 'when passing values as args' do
+      context 'when passing the sport' do
+        it 'should request that sport' do
+          @client.sports(:baseball)
+          assert_requested :get, espn_url('sports/baseball')
+        end
+      end
+
+      context 'when passing the league' do
+        it 'should request that league' do
+          @client.sports(:mlb)
+          assert_requested :get, espn_url('sports/baseball/mlb')
+        end
+      end
+
+      context 'when passing the sport and league' do
+        it 'should request that sport and league' do
+          @client.sports(:baseball, :nba)
+          assert_requested :get, espn_url('sports/baseball/nba')
+        end
+      end
+
+      context 'when passing the league in the opts hash' do
+        it 'should override the arg' do
+          @client.sports(:nba, league: 'mlb')
+          assert_requested :get, espn_url('sports/basketball/mlb')
+        end
+      end
+
+      context 'when passing the sport in the opts hash' do
+        it 'should override the arg' do
+          @client.sports(:baseball, sport: :basketball)
+          assert_requested :get, espn_url('sports/basketball')
+        end
       end
     end
 
-    context 'with a sport and a league param' do
-      it 'should include both params in the request' do
-        @client.sports(sport: 'baseball', league: 'mlb')
-        assert_requested :get, espn_url('sports/baseball/mlb')
+    context 'when passing values in the opts hash' do
+      context 'with a sport param' do
+        it 'should include the sport param in the request' do
+          @client.sports(sport: 'baseball')
+          assert_requested :get, espn_url('sports/baseball')
+        end
+      end
+
+      context 'with a sport and a league param' do
+        it 'should include both params in the request' do
+          @client.sports(sport: 'baseball', league: 'mlb')
+          assert_requested :get, espn_url('sports/baseball/mlb')
+        end
+      end
+
+      context 'with a league param but no sport param' do
+        it 'should map the league to a sport' do
+          @client.sports(league: 'mlb')
+          assert_requested :get, espn_url('sports/baseball/mlb')
+        end
       end
     end
-
-    context 'with a league param but no sport param' do
-      it 'should not include the league param in the request' do
-        @client.sports(league: 'mlb')
-        assert_requested :get, espn_url('sports')
-      end
-    end
-
   end
 end
