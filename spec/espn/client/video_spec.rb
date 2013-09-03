663dd195b37f572574ccd87c581c4e36813de23a
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-03 08:34:09 -0500

    updates to #video, changed to #videos

--- spec/espn/client/video_spec.rb
+++ spec/espn/client/video_spec.rb
@@ -6,21 +6,25 @@ describe ESPN::Client::Video do
     stub_get 'videos.json'
   end
 
+  it 'should return an array of channels' do
+    @client.videos.first.name.should eq('This is a video channel.')
+  end
+
   it 'should get all clips' do
-    @client.video
+    @client.videos
     assert_requested :get, espn_url('video/channels/clips')
   end
 
   context 'with a category_id param' do
     it 'should include the category_id in the request' do
-      @client.video(category_id: 1)
+      @client.videos(category_id: 1)
       assert_requested :get, espn_url('video/channels/1/clips')
     end
   end
 
   context 'with a clip_id param' do
     it 'should include the clip_id in the request' do
-      @client.video(clip_id: 2)
+      @client.videos(clip_id: 2)
       assert_requested :get, espn_url('video/channels/clips/2')
     end
   end
