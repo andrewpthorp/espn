24442ad262a73719526a5c88a4751811a84a0366
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-03 11:00:43 -0500

    added #athlete

--- lib/espn/client/athletes.rb
+++ lib/espn/client/athletes.rb
@@ -10,6 +10,38 @@ module ESPN
     #   end
     module Athletes
 
+      # Public: Get a specific athlete from the ESPN API.
+      #
+      # id    - An id of an athlete.
+      # sport   - A Symbol or String of the sport (optional).
+      # league  - A Symbol or String of the league. If a league is passed, but
+      #           not a sport, an attempt will be made to map the league to a
+      #           sport (required).
+      # opts    - Hash options used to refine the selection. If sport and/or
+      #           league are passed in, they will override the mapped values
+      #           You can find a full list of options on the ESPN developer API
+      #           website (default: {}).
+      #
+      # Returns a Hashie::Mash.
+      def athlete(id, *args)
+        # Build fragments and options
+        opts = extract_options_with_defaults!(args)
+        opts[:sport], opts[:league] = extract_sport_and_league(args, opts)
+
+        if blank?(opts[:league])
+          raise ArgumentError, 'You must provide a valid league.'
+        end
+
+        # Build URL
+        pattern = "sports/:sport/:league/athletes/#{id}"
+        url = build_url(pattern, opts)
+
+        puts url
+
+        # Make request
+        get(url, opts).sports.first.leagues.first.athletes.first
+      end
+
       # Public: Get athlete stats and information from the ESPN API.
       #
       # sport   - A Symbol or String of the sport (optional).
@@ -20,7 +52,6 @@ module ESPN
       #           league are passed in, they will override the mapped values
       #           You can find a full list of options on the ESPN developer API
       #           website (default: {}).
-      #           :id - The id of a specific athlete (default: nil, optional).
       #
       # Returns an Array of Hashie::Mash.
       def athletes(*args)
@@ -33,7 +64,7 @@ module ESPN
         end
 
         # Build URL
-        pattern = "sports/:sport/:league/athletes/:id"
+        pattern = "sports/:sport/:league/athletes"
         url = build_url(pattern, opts)
 
         # Make request
--- spec/espn/client/athletes_spec.rb
+++ spec/espn/client/athletes_spec.rb
@@ -6,6 +6,34 @@ describe ESPN::Client::Athletes do
     stub_get 'athletes.json'
   end
 
+  describe '#athlete' do
+    it 'should return an athlete' do
+      @client.athlete(1, :mlb).fullName.should eq('Chase Utley')
+    end
+
+    it 'should get an athlete for the given league' do
+      @client.athlete(1, :mlb)
+      assert_requested :get, espn_url('sports/baseball/mlb/athletes/1')
+    end
+
+    it 'should accept a league in the opts hash' do
+      @client.athlete(1, league: 'mlb')
+      assert_requested :get, espn_url('sports/baseball/mlb/athletes/1')
+    end
+
+    context 'when league is not passed' do
+      it 'should raise an argument error' do
+        expect { @client.athlete(1) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when invalid league is passed' do
+      it 'should raise an argument error' do
+        expect { @client.athlete(1, :foobar) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
   describe '#athletes' do
     it 'should return an array of athletes' do
       @client.athletes(:mlb).first.fullName.should eq('Chase Utley')
@@ -30,13 +58,6 @@ describe ESPN::Client::Athletes do
     context 'when invalid league is passed' do
       it 'should raise an argument error' do
         expect { @client.athletes(:foobar) }.to raise_error(ArgumentError)
-      end
-    end
-
-    context 'when passing an id in the opts hash' do
-      it 'should request only that athlete' do
-        @client.athletes(:mlb, id: 1)
-        assert_requested :get, espn_url('sports/baseball/mlb/athletes/1')
       end
     end
   end
