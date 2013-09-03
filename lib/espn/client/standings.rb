49e53dace2ed3c23919f4f939c5a14551667fd5a
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-03 08:11:39 -0500

    updates to #standings

--- lib/espn/client/standings.rb
+++ lib/espn/client/standings.rb
@@ -1,27 +1,38 @@
 module ESPN
   class Client
+
+    # Public: The mapping to the STANDINGS API of the ESPN developer API.
+    #
+    # Examples
+    #
+    #   class Client
+    #     include ESPN::Client::Standings
+    #   end
     module Standings
 
       # Public: Get the latest league and divisional standings from ESPN.
       #
-      # opts  - Hash options used to refine the selection (default: {}).
-      #       - :sport    - The name of the sport (default: nil).
-      #       - :league   - The name of the league (default: nil).
+      # sport   - A Symbol or String of the sport (optional).
+      # league  - A Symbol or String of the league. If a league is passed, but
+      #           not a sport, an attempt will be made to map the league to a
+      #           sport (required).
+      # opts    - Hash options used to refine the selection. If sport and/or
+      #           league are passed in, they will override the mapped values
+      #           You can find a full list of options on the ESPN developer API
+      #           website (default: {}).
       #
       # Returns an Array of Hashie::Mash.
-      def standings(opts={})
-        url = 'sports'
+      def standings(*args)
+        # Build fragments and options
+        opts = extract_options_with_defaults!(args)
+        opts[:sport], opts[:league] = extract_sport_and_league(args, opts)
 
-        unless opts[:sport].to_s.empty?
-          url += "/#{opts[:sport]}"
-          unless opts[:league].to_s.empty?
-            url += "/#{opts[:league]}"
-          end
-        end
+        # Build URL
+        pattern = 'sports/:sport/:league/standings'
+        url = build_url(pattern, opts)
 
-        url += '/standings'
-
-        get(url, opts)
+        # Make request
+        get(url, opts).sports.first.leagues.first.groups
       end
 
     end
--- spec/espn/client/standings_spec.rb
+++ spec/espn/client/standings_spec.rb
@@ -7,32 +7,60 @@ describe ESPN::Client::Standings do
   end
 
   describe '#standings' do
+    it 'returns an array of standings' do
+      team = @client.standings.first.groups.first
+      team.competitors.is_a?(Array).should be_true
+    end
 
     it 'gets all standings' do
       @client.standings
       assert_requested :get, espn_url('sports/standings')
     end
 
-    context 'with a sport param' do
-      it 'should include the sport in the request' do
-        @client.standings(sport: 'baseball')
-        assert_requested :get, espn_url('sports/baseball/standings')
+    context 'when passing values as args' do
+      context 'when passing the sport' do
+        it 'should get standings for that sport' do
+          @client.standings(:baseball)
+          assert_requested :get, espn_url('sports/baseball/standings')
+        end
+      end
+
+      context 'when passing the league' do
+        it 'should map that league to a sport' do
+          @client.standings(:mlb)
+          assert_requested :get, espn_url('sports/baseball/mlb/standings')
+        end
+      end
+
+      context 'when passing the sport and league' do
+        it 'should get standings for that sport and league' do
+          @client.standings(:football, :nfl)
+          assert_requested :get, espn_url('sports/football/nfl/standings')
+        end
       end
     end
 
-    context 'with a sport and a league param' do
-      it 'should include the sport and league params in the request' do
-        @client.standings(sport: 'baseball', league: 'mlb')
-        assert_requested :get, espn_url('sports/baseball/mlb/standings')
+    context 'when passing values in the opts hash' do
+      context 'with a sport param' do
+        it 'should include the sport in the request' do
+          @client.standings(sport: 'baseball')
+          assert_requested :get, espn_url('sports/baseball/standings')
+        end
+      end
+
+      context 'with a sport and a league param' do
+        it 'should include the sport and league params in the request' do
+          @client.standings(sport: 'baseball', league: 'mlb')
+          assert_requested :get, espn_url('sports/baseball/mlb/standings')
+        end
+      end
+
+      context 'with a league param and no sport param' do
+        it 'should map the league to a sport' do
+          @client.standings(league: 'mlb')
+          assert_requested :get, espn_url('sports/baseball/mlb/standings')
+        end
       end
     end
-
-    context 'with a league param and no sport param' do
-      it 'should not include either in the request' do
-        @client.standings(league: 'mlb')
-        assert_requested :get, espn_url('sports/standings')
-      end
-    end
-
   end
 end
